4842aaccc9d1f953839af8464e3982f578e38f67
daumaydemoctoca <quang@DESKTOP-SN5Q3DL.localdomain>
2019-12-18 17:19:06 +0700

    but na is pro

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Khoi and quang is noob");
+        System.out.println("Khoi and quang is noob, na is pro");
     }
 }
